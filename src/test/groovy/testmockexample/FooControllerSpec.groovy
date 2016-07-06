333b5a792c4db40dc47336239d01c8080f93611e
Colin Harrington <colin.harrington@gmail.com>
2016-07-06 11:13:42 -0500

    Test with a real instance

--- src/test/groovy/testmockexample/FooControllerSpec.groovy
+++ src/test/groovy/testmockexample/FooControllerSpec.groovy
@@ -2,15 +2,17 @@ package testmockexample
 
 import grails.orm.PagedResultList
 import grails.test.mixin.TestFor
+import org.hibernate.Criteria
 import spock.lang.Specification
 
 @TestFor(FooController)
 class FooControllerSpec extends Specification {
 
-    void "sample Test with map"() {
+    void "sample Test with a real instance"() {
         given:
             controller.sampleService = Mock(SampleService)
-            PagedResultList emptyPagedResultList = [empty: true] as PagedResultList
+            Criteria criteria = [list:{->[]}] as Criteria
+            PagedResultList emptyPagedResultList = new PagedResultList(null, criteria)
 
         when:
             controller.testFunction()
